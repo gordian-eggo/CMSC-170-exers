3abb34b3f2a60239a6b0ccdc7c3d9e02d02a9d74
gordian-eggo <columbus.knot@gmail.com>
2019-02-02 21:37:28 +0800

    Edited State class.

--- RuzEx02v01/src/State.java
+++ RuzEx02v01/src/State.java
@@ -6,7 +6,7 @@ import java.io.FileNotFoundException;
 public class State {
 
 	protected String[][] current_board = new String[5][5];
-	protected String[][] parent_board  = new String[5][5];
+	protected State parent;
 	protected int cost;
 	protected boolean is_level_2_state;
 
@@ -15,29 +15,30 @@ public class State {
 	public int get_cost() { return this.cost; }
 	public boolean get_is_level_2_state() { return this.is_level_2_state; }
 	public String[][] get_current_board() { return this.current_board; }
-	public String[][] get_parent_board() { return this.parent_board; }
+	public State get_parent_board() { return this.parent; }
 
 	// setters
 	public void set_cost(int c) { this.cost = c; }
 	public void set_is_level_2_state(boolean b) { this.is_level_2_state = b; }
-	public void set_parent_board(File f) { 
+	public void set_parent(State p) { this.parent = p; }
+	// public void set_parent_board(File f) { 
 		
-		try {
+	// 	try {
 
-			Scanner s = new Scanner(f);
+	// 		Scanner s = new Scanner(f);
 
-			while(s.hasNext()) {								// populate the temporary 
-				for (int row = 0; row < 5; row++) {
-					for (int col = 0; col < 5; col++) {
-						this.current_board[row][col] = s.next();
-					}
-				}
-			}
+	// 		while(s.hasNext()) {								// populate the temporary 
+	// 			for (int row = 0; row < 5; row++) {
+	// 				for (int col = 0; col < 5; col++) {
+	// 					this.current_board[row][col] = s.next();
+	// 				}
+	// 			}
+	// 		}
 
-		} catch (Exception e) {}
+	// 	} catch (Exception e) {}
 
-	}
-	public void set_parent_board(State s) { this.parent_board = s.current_board; }
+	// }
+	// public void set_parent_board(State s) { this.parent_board = s.current_board; }
 	public void set_current_board(GUI g) {
 
 		try {
